7978d9ae687e7705dbcf3be47dfa5255f088a356
Vladimir <kapitonov.vladimir@list.ru>
2024-09-21 20:01:07 +1000

    add test file

--- test/test_protocol.py
+++ test/test_protocol.py
@@ -1,3 +1,5 @@
+import os
+from pathlib import Path
 from src.test_protocol import TestProtocol
 
 LINES = [
@@ -12,11 +14,24 @@ RESULTS = [
     {'code_msg': 3, 'datetime': 1726813483, 'imei': '869586748696585', 'lat': 53.54650115966797, 'lon': 49.34349822998047, 'num_pack': 2, 'type_message': 2},
 ]
 
+BASE_PATH = os.path.join(Path(__file__).absolute().parents[1], Path("test/input_data.txt"))
+
+
+def _comparison_results(position: int, data: dict) -> None:
+    assert not set(data.keys()) - set(RESULTS[position].keys()), f"Строка {position} не соответствуют ключи"
+    assert not set(RESULTS[position].keys() - data.keys()), f"Строка {position} не соответствуют ключи"
+    assert not set(data.values()) - set(RESULTS[position].values()), f"Строка {position} не соответствуют значения"
+    assert not set(RESULTS[position].values()) - set(data.values()), f"Строка {position} не соответствуют значения"
+
 
 def test_pars_lines():
     for position, line in enumerate(LINES):
         data = TestProtocol.parse(line)
-        assert not set(data.keys()) - set(RESULTS[position].keys()), f"Строка {position} не соответствуют ключи"
-        assert not set(RESULTS[position].keys() - data.keys()), f"Строка {position} не соответствуют ключи"
-        assert not set(data.values()) - set(RESULTS[position].values()), f"Строка {position} не соответствуют значения"
-        assert not set(RESULTS[position].values()) - set(data.values()), f"Строка {position} не соответствуют значения"
+        _comparison_results(position, data)
+
+
+def test_pars_file():
+    with open(BASE_PATH, "rb") as f:
+        for position, line in enumerate(f.readlines()):
+            data = TestProtocol.parse(line.strip())
+            _comparison_results(position, data)
